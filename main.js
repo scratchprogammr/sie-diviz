be5dd0560ef420efe76486dd579174ce5adbf879
scratchprogammr <131555910+scratchprogammr@users.noreply.github.com>
2023-06-09 11:14:27 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+var a = 'startup'
+var b = 'flags'
+var c = 'exit'
+//core var
+function start(a, b, c){
+  var startup = start(a, b, c)
+  var flags = var c/a
+  var exit = return 0;
+}
+start(a, b, c)
